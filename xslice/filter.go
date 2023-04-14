5a6986c5532a11510b784adb426bfa640c535412
xwzhou <xwzhou@yeah.net>
2023-04-14 10:47:39 +0800

    update filter

--- xslice/filter.go
+++ xslice/filter.go
@@ -8,25 +8,25 @@ func Filter(length int, f func(i int) bool, push func(i int)) {
 	}
 }
 
-func FilterToInts(arr []int, f func(i int) bool) []int {
+func FilterInts(arr []int, f func(i int) bool) []int {
 	var dst []int
 	Filter(len(arr), f, func(i int) { dst = append(dst, arr[i]) })
 	return dst
 }
 
-func FilterToInt32s(arr []int32, f func(i int) bool) []int32 {
+func FilterInt32s(arr []int32, f func(i int) bool) []int32 {
 	var dst []int32
 	Filter(len(arr), f, func(i int) { dst = append(dst, arr[i]) })
 	return dst
 }
 
-func FilterToInt64s(arr []int64, f func(i int) bool) []int64 {
+func FilterInt64s(arr []int64, f func(i int) bool) []int64 {
 	var dst []int64
 	Filter(len(arr), f, func(i int) { dst = append(dst, arr[i]) })
 	return dst
 }
 
-func FilterToStrings(arr []string, f func(i int) bool) []string {
+func FilterStrings(arr []string, f func(i int) bool) []string {
 	var dst []string
 	Filter(len(arr), f, func(i int) { dst = append(dst, arr[i]) })
 	return dst
