e13c9e092db86ec23905d3685595ed71cfe4c013
xwzhou <xwzhou@yeah.net>
2023-07-17 14:07:31 +0800

    FIX Filter

--- xslice/filter.go
+++ xslice/filter.go
@@ -1,11 +1,28 @@
 package xslice
 
-func Filter(length int, f func(i int) bool, push func(i int)) {
+func FilterStable(length int, f func(i int) bool, push func(i int)) {
 	for i := 0; i < length; i++ {
 		if f(i) {
 			push(i)
 		}
 	}
+}
+
+func FilterUnstable(length int, f func(i int) bool, swap func(i int, j int)) int {
+	var j = 0
+	for i := 0; i < length; i++ {
+		if f(i) {
+			if i != j {
+				swap(i, j)
+			}
+			j++
+		}
+	}
+	return j
+}
+
+func Filter(length int, f func(i int) bool, push func(i int)) {
+	FilterStable(length, f, push)
 }
 
 func FilterInts(arr []int, f func(i int) bool) []int {
@@ -30,4 +47,24 @@ func FilterStrings(arr []string, f func(i int) bool) []string {
 	var dst []string
 	Filter(len(arr), f, func(i int) { dst = append(dst, arr[i]) })
 	return dst
+}
+
+func FilterUnstableInts(arr []int, f func(i int) bool) []int {
+	p := FilterUnstable(len(arr), f, func(i, j int) { arr[i], arr[j] = arr[j], arr[i] })
+	return arr[:p]
+}
+
+func FilterUnstableInt32s(arr []int32, f func(i int) bool) []int32 {
+	p := FilterUnstable(len(arr), f, func(i, j int) { arr[i], arr[j] = arr[j], arr[i] })
+	return arr[:p]
+}
+
+func FilterUnstableInt64s(arr []int64, f func(i int) bool) []int64 {
+	p := FilterUnstable(len(arr), f, func(i, j int) { arr[i], arr[j] = arr[j], arr[i] })
+	return arr[:p]
+}
+
+func FilterUnstableStrings(arr []string, f func(i int) bool) []string {
+	p := FilterUnstable(len(arr), f, func(i, j int) { arr[i], arr[j] = arr[j], arr[i] })
+	return arr[:p]
 }
--- xslice/filter_test.go
+++ xslice/filter_test.go
@@ -25,3 +25,27 @@ func TestFilter(t *testing.T) {
 	}
 
 }
+
+func TestFilterUnstable(t *testing.T) {
+
+	var cases = []struct {
+		Param1 []int
+		Param2 int
+
+		Want []int
+	}{
+		{Param1: []int{0, 1, 2, 3, 4, 5, 6, 7}, Param2: 1, Want: []int{2, 3, 4, 5, 6, 7}},
+		{Param1: []int{0, 1, 2, 3, 4, 5, 6, 7}, Param2: 2, Want: []int{3, 4, 5, 6, 7}},
+		{Param1: []int{0, 1, 2, 3, 4, 5, 6, 7}, Param2: 3, Want: []int{4, 5, 6, 7}},
+		{Param1: []int{0, 1, 2, 3, 4, 5, 6, 7}, Param2: 4, Want: []int{5, 6, 7}},
+		{Param1: []int{0, 1, 2, 3, 4, 5, 6, 7}, Param2: 5, Want: []int{6, 7}},
+	}
+
+	for _, c := range cases {
+		got := FilterUnstableInts(c.Param1, func(i int) bool { return c.Param1[i] > c.Param2 })
+		if !EqualInts(c.Want, got) {
+			t.Fatalf("param1: %v, want: %v, but got: %v", c.Param1, c.Want, got)
+		}
+	}
+
+}
