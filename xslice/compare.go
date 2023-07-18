ca148c600cb149e04afb09f92a8303804177079f
xwzhou <xwzhou@yeah.net>
2023-07-18 10:01:46 +0800

    add swapper

--- xslice/compare.go
+++ xslice/compare.go
@@ -2,7 +2,7 @@ package xslice
 
 import "time"
 
-func CompareInt(a int, b int) int {
+func CompareInt8(a int8, b int8) int {
 	if a == b {
 		return 0
 	}
@@ -12,7 +12,17 @@ func CompareInt(a int, b int) int {
 	return +1
 }
 
-func CompareUint(a uint, b uint) int {
+func CompareInt16(a int16, b int16) int {
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return +1
+}
+
+func CompareInt(a int, b int) int {
 	if a == b {
 		return 0
 	}
@@ -42,6 +52,36 @@ func CompareInt64(a int64, b int64) int {
 	return +1
 }
 
+func CompareUint8(a uint8, b uint8) int {
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return +1
+}
+
+func CompareUint16(a uint16, b uint16) int {
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return +1
+}
+
+func CompareUint(a uint, b uint) int {
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return +1
+}
+
 func CompareUint32(a uint32, b uint32) int {
 	if a == b {
 		return 0
@@ -53,6 +93,16 @@ func CompareUint32(a uint32, b uint32) int {
 }
 
 func CompareUint64(a uint64, b uint64) int {
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return +1
+}
+
+func CompareByte(a byte, b byte) int {
 	if a == b {
 		return 0
 	}
---
+++ xslice/swapper.go
@@ -0,0 +1,72 @@
+package xslice
+
+import (
+	"reflect"
+	"time"
+)
+
+var Swapper func(slice interface{}) func(int, int) = reflect.Swapper
+
+func SwapInt8s(slice []int8) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapInt16s(slice []int16) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapInts(slice []int) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapInt32s(slice []int32) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapInt64s(slice []int64) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapUint8s(slice []int8) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapUint16s(slice []uint16) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapUints(slice []uint) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapUint32s(slice []uint32) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapUint64s(slice []uint64) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapFloat32s(slice []float32) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapFloat64s(slice []float64) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapBytes(slice []byte) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapStrings(slice []int64) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapTimes(slice []time.Time) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
+
+func SwapDurations(slice []time.Duration) func(int, int) {
+	return func(i, j int) { slice[i], slice[j] = slice[j], slice[i] }
+}
