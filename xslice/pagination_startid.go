b77270ccf573ede0d2e0dcf652a0b88890e49578
xwzhou <xwzhou@yeah.net>
2024-05-31 10:02:28 +0800

    rename PaginationID to PaginationStartID

--- xslice/pagination_startid.go
+++ xslice/pagination_startid.go
@@ -5,8 +5,8 @@ import "context"
 // example:
 //
 //	var ls []string
-//	_, err := PaginationID(context.TODO(), 10, func(startID int64, limit int64) (lastID int64, n int64, err error) {
-//		var tmp []int64
+//	_, err := PaginationStartID(context.TODO(), 10, func(startID int64, limit int64) (lastID int64, n int64, err error) {
+//		var tmp []string
 //		query(&tmp,"... where id >= ? limit ?", startID, limit)
 //		if len(tmp) == 0 {
 //			return startID, 0, nil
@@ -15,11 +15,11 @@ import "context"
 //		ls = append(ls, tmp...)
 //		return lastID, int64(len(tmp)), nil
 //	})
-func PaginationID(ctx context.Context, limit int64, fn func(startID int64, limit int64) (lastID int64, n int64, err error)) (total int64, err error) {
-	return PaginationIDWithParams(ctx, 0 /*startID*/, limit /*limit*/, -1 /*maxTotal*/, fn)
+func PaginationStartID(ctx context.Context, limit int64, fn func(startID int64, limit int64) (lastID int64, n int64, err error)) (total int64, err error) {
+	return PaginationStartIDWithParams(ctx, 0 /*startID*/, limit /*limit*/, -1 /*maxTotal*/, fn)
 }
 
-func PaginationIDWithParams(ctx context.Context, startID int64, limit int64, maxTotal int64, fn func(startID int64, limit int64) (lastID int64, n int64, err error)) (total int64, err error) {
+func PaginationStartIDWithParams(ctx context.Context, startID int64, limit int64, maxTotal int64, fn func(startID int64, limit int64) (lastID int64, n int64, err error)) (total int64, err error) {
 	if limit < 0 {
 		limit = 10
 	}
