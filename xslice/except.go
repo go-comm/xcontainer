3aa631c3458c6b686b1d7925c329de3b4481e9f2
xwzhou <xwzhou@yeah.net>
2024-06-25 21:21:31 +0800

    add ExceptSorted

--- xslice/except.go
+++ xslice/except.go
@@ -21,16 +21,33 @@ func ExceptStable(lenA int, lenB int, equal func(ia int, ib int) bool, push func
 }
 
 // ExceptUnstable returns the elements in `a` that aren't in `b`. a-(a∩b)
-func ExceptUnstable(lenA int, lenB int, swap func(i int, j int), equal func(ia int, ib int) bool) int {
+func ExceptUnstable(lenA int, lenB int, swapA func(i int, j int), equal func(ia int, ib int) bool) int {
 	hi := lenA
 	for i := hi - 1; i >= 0; i-- {
 		for j := 0; j < lenB; j++ {
 			if equal(i, j) {
 				if i != hi-1 {
-					swap(i, hi-1)
+					swapA(i, hi-1)
 				}
 				hi--
 				break
+			}
+		}
+	}
+	return hi
+}
+
+// ExceptSorted returns the elements in `a` that aren't in `b`. a-(a∩b)
+func ExceptSorted(lenA int, lenSortedB int, swapA func(i int, j int), cmp func(ia int, ib int) int) int {
+	hi := lenA
+	if lenSortedB > 0 {
+		for i := hi - 1; i >= 0; i-- {
+			_, ok := BinaryFind(lenSortedB, func(j int) int { return cmp(i, j) })
+			if ok {
+				if i != hi-1 {
+					swapA(i, hi-1)
+				}
+				hi--
 			}
 		}
 	}
@@ -68,4 +85,32 @@ func ExceptStrings(a []string, b []string) []string {
 	var c []string
 	ExceptStable(len(a), len(b), func(ia, ib int) bool { return a[ia] == b[ib] }, func(ia int) { c = append(c, a[ia]) })
 	return c
+}
+
+// ExceptUnstableInts returns the elements in `a` that aren't in `b`. a-(a∩b)
+func ExceptUnstableInts(a []int, b []int) []int {
+	SortInts(b)
+	p := ExceptSorted(len(a), len(b), func(i, j int) { a[i], a[j] = a[j], a[i] }, func(ia, ib int) int { return CompareInt(a[ia], b[ib]) })
+	return a[:p]
+}
+
+// ExceptUnstableInt32s returns the elements in `a` that aren't in `b`. a-(a∩b)
+func ExceptUnstableInt32s(a []int32, b []int32) []int32 {
+	SortInt32s(b)
+	p := ExceptSorted(len(a), len(b), func(i, j int) { a[i], a[j] = a[j], a[i] }, func(ia, ib int) int { return CompareInt32(a[ia], b[ib]) })
+	return a[:p]
+}
+
+// ExceptUnstableInt64s returns the elements in `a` that aren't in `b`. a-(a∩b)
+func ExceptUnstableInt64s(a []int64, b []int64) []int64 {
+	SortInt64s(b)
+	p := ExceptSorted(len(a), len(b), func(i, j int) { a[i], a[j] = a[j], a[i] }, func(ia, ib int) int { return CompareInt64(a[ia], b[ib]) })
+	return a[:p]
+}
+
+// ExceptUnstableStrings returns the elements in `a` that aren't in `b`. a-(a∩b)
+func ExceptUnstableStrings(a []string, b []string) []string {
+	SortStrings(b)
+	p := ExceptSorted(len(a), len(b), func(i, j int) { a[i], a[j] = a[j], a[i] }, func(ia, ib int) int { return CompareString(a[ia], b[ib]) })
+	return a[:p]
 }
--- xslice/except_test.go
+++ xslice/except_test.go
@@ -24,3 +24,26 @@ func TestExceptInts(t *testing.T) {
 	}
 
 }
+
+func TestExceptUnstableInts(t *testing.T) {
+
+	var cases = []struct {
+		Param1 []int
+		Param2 []int
+		Want   []int
+	}{
+		{Param1: []int{1, 2, 3, 4, 5, 6, 6, 8, 8}, Param2: []int{1, 2, 3, 4, 5, 6, 8}, Want: []int{}},
+		{Param1: []int{1, 2, 3, 4, 5, 6, 6, 8, 8}, Param2: []int{1, 2, 3, 4, 5, 6, 6}, Want: []int{8, 8}},
+		{Param1: []int{1, 2, 3, 4, 5, 6, 6, 8, 8}, Param2: []int{1, 2, 8}, Want: []int{3, 4, 5, 6, 6}},
+		{Param1: []int{1, 2, 3, 4, 5, 6, 6, 8, 8}, Param2: []int{5, 6, 8}, Want: []int{1, 2, 3, 4}},
+	}
+
+	for _, c := range cases {
+		got := ExceptUnstableInts(c.Param1, c.Param2)
+		got = SortInts(got)
+		if !EqualInts(c.Want, got) {
+			t.Fatalf("param1: %v, param2: %v, want: %v, but got: %v", c.Param1, c.Param2, c.Want, got)
+		}
+	}
+
+}
